c0d4975ffa4c8aa74244ea0a154c7889d40fa0e3
Eugene Pisarev <zm1s3ry@gmail.com>
2024-12-18 22:34:34 +0300

    added schedule and salon models

--- beauty_bot/bot/admin.py
+++ beauty_bot/bot/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Salon, Schedule, Specialist
 
-# Register your models here.
+admin.site.register(Salon)
+admin.site.register(Schedule)
+admin.site.register(Specialist)
--- beauty_bot/bot/models.py
+++ beauty_bot/bot/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Specialist(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Имя мастера")
+
+    class Meta:
+        verbose_name = "Мастер"
+        verbose_name_plural = "Мастера"
+
+    def __str__(self):
+        return self.name
+
+
+class Salon(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название салона")
+    address = models.TextField(verbose_name="Адрес")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    email = models.EmailField(verbose_name="Электронная почта", blank=True, null=True)
+    opening_time = models.TimeField(verbose_name="Время открытия")
+    closing_time = models.TimeField(verbose_name="Время закрытия")
+
+    class Meta:
+        verbose_name = "Салон"
+        verbose_name_plural = "Салоны"
+
+    def __str__(self):
+        return self.name
+
+
+class Schedule(models.Model):
+    salon = models.ForeignKey(Salon, on_delete=models.CASCADE, verbose_name="Салон", related_name="schedules")
+    specialist = models.ForeignKey('Specialist', on_delete=models.CASCADE, verbose_name="Специалист", related_name="schedules")
+    # задел для связки с моделью специалиста
+    date = models.DateField(verbose_name="Дата")
+    start_time = models.TimeField(verbose_name="Время начала")
+    end_time = models.TimeField(verbose_name="Время окончания")
+
+    class Meta:
+        verbose_name = "Расписание"
+        verbose_name_plural = "Расписания"
+        unique_together = ('specialist', 'date', 'start_time')
+
+    def __str__(self):
+        return f"{self.specialist} - {self.date} ({self.start_time} - {self.end_time})"
